c1ae7654f3e9c60d0080518ddbbcee0bcf51d263
Scott Andrews <scott@andrews.me>
2026-06-12 11:14:16 -0400

    wasi 0.3.0
    This is a breaking change from wasi@0.2.x unless an adapter is applied.
    
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- components/stderr-to-stdout/src/lib.rs
+++ components/stderr-to-stdout/src/lib.rs
@@ -1,20 +1,31 @@
 #![no_main]
 
-use crate::exports::wasi::cli::stderr::Guest;
-use crate::wasi::cli::stdout::get_stdout;
-use crate::wasi::io::streams::OutputStream;
+use crate::exports::wasi::cli::stderr::{ErrorCode, Guest};
+use crate::wasi::cli::stdout;
 
 pub(crate) struct StderrToStdout;
 
 impl Guest for StderrToStdout {
-    fn get_stderr() -> OutputStream {
-        get_stdout()
+    #[doc = "/ Write the given stream to stderr."]
+    #[doc = "/"]
+    #[doc = "/ If the stream\'s writable end is dropped this function will either return"]
+    #[doc = "/ success once the entire contents of the stream have been written or an"]
+    #[doc = "/ error-code representing a failure."]
+    #[doc = "/"]
+    #[doc = "/ Otherwise if there is an error the readable end of the stream will be"]
+    #[doc = "/ dropped and this function will return an error-code."]
+    #[allow(async_fn_in_trait)]
+    fn write_via_stream(
+        data: wit_bindgen::rt::async_support::StreamReader<u8>,
+    ) -> wit_bindgen::rt::async_support::FutureReader<Result<(), ErrorCode>> {
+        stdout::write_via_stream(data)
     }
 }
 
 wit_bindgen::generate!({
     path: "../../wit",
     world: "stderr-to-stdout",
+    merge_structurally_equal_types: true,
     generate_all
 });
 
--- components/stdout-to-stderr/src/lib.rs
+++ components/stdout-to-stderr/src/lib.rs
@@ -1,20 +1,31 @@
 #![no_main]
 
-use crate::exports::wasi::cli::stdout::Guest;
-use crate::wasi::cli::stderr::get_stderr;
-use crate::wasi::io::streams::OutputStream;
+use crate::exports::wasi::cli::stdout::{ErrorCode, Guest};
+use crate::wasi::cli::stderr;
 
 pub(crate) struct StdoutToStderr;
 
 impl Guest for StdoutToStderr {
-    fn get_stdout() -> OutputStream {
-        get_stderr()
+    #[doc = "/ Write the given stream to stdout."]
+    #[doc = "/"]
+    #[doc = "/ If the stream\'s writable end is dropped this function will either return"]
+    #[doc = "/ success once the entire contents of the stream have been written or an"]
+    #[doc = "/ error-code representing a failure."]
+    #[doc = "/"]
+    #[doc = "/ Otherwise if there is an error the readable end of the stream will be"]
+    #[doc = "/ dropped and this function will return an error-code."]
+    #[allow(async_fn_in_trait)]
+    fn write_via_stream(
+        data: wit_bindgen::rt::async_support::StreamReader<u8>,
+    ) -> wit_bindgen::rt::async_support::FutureReader<Result<(), ErrorCode>> {
+        stderr::write_via_stream(data)
     }
 }
 
 wit_bindgen::generate!({
     path: "../../wit",
     world: "stdout-to-stderr",
+    merge_structurally_equal_types: true,
     generate_all
 });
 
